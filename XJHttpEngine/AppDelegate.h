5f5a71ccc013ae329775aa662d33fc994893c897
ShiXianjun <1092207960@qq.com>
2019-07-01 10:30:22 +0800

    新建项目

---
+++ XJHttpEngine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XJHttpEngine
+//
+//  Created by 石显军 on 2019/7/1.
+//  Copyright © 2019 石显军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XJHttpEngine/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XJHttpEngine
+//
+//  Created by 石显军 on 2019/7/1.
+//  Copyright © 2019 石显军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
